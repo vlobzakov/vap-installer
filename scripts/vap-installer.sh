57671f9d20631354fd54fdb08d2e99f9f0bb658e
sych74 <sych74@gmail.com>
2023-03-09 17:51:43 +0100

    Update vap-installer.sh

--- scripts/vap-installer.sh
+++ scripts/vap-installer.sh
@@ -75,7 +75,7 @@ getFlavorsByParam(){
       id=$((id+1))
       Name=$(_jq '.Name')
       Ephemeral=$(_jq '.Ephemeral')
-      
+
       infra_flavors=$(echo $infra_flavors | jq \
         --argjson id "$id" \
         --arg Name "$Name" \
@@ -85,7 +85,7 @@ getFlavorsByParam(){
       '. += [{"id": $id, "Name": $Name, "RAM": $RAM, "VCPUs": $VCPUs, "Ephemeral": $Ephemeral}]')
     }
   done
-  
+
   if [[ "x${FORMAT}" == "xjson" ]]; then
     output="{\"result\": 0, \"${name}\": ${infra_flavors}}"
     echo $output > ${name}.json
@@ -93,7 +93,7 @@ getFlavorsByParam(){
     seperator=---------------------------------------------------------------------------------------------------
     rows="%-5s| %-20s| %-20s| %-20s| %s\n"
     TableWidth=100
-    echo "${title}"
+    echo -e "\n\n${title}"
     printf "%.${TableWidth}s\n" "$seperator"
     printf "%-5s| %-20s| %-20s| %-20s| %s\n" ID Name RAM VCPUs Ephemeral
     printf "%.${TableWidth}s\n" "$seperator"
@@ -119,6 +119,54 @@ getInfraFlavors(){
 
 getUserFlavors(){
   getFlavorsByParam "userFlavors" "${MIN_USER_VCPU}" "${MIN_USER_RAM}" "User node flavors"
+}
+
+getImages(){
+  local id=0
+  local images=$(jq -n '[]')
+  local cmd="${OPENSTACK} image list -f json"
+  local full_images=$(execReturn "${cmd}" "Getting images list")
+
+  for image in $(echo "${full_images}" | jq -r '.[] | @base64'); do
+    _jq() {
+       echo "${image}" | base64 --decode | jq -r "${1}"
+    }
+    Name=$(_jq '.Name')
+    grep -E "^vap-[0-9]{2}-[0-9]_[0-9]{14}" <<< $Name && {
+      id=$((id+1))
+      Status=$(_jq '.Status')
+
+      images=$(echo $images | jq \
+        --argjson id "$id" \
+        --arg Name "$Name" \
+        --arg Status  "$Status" \
+      '. += [{"id": $id, "Name": $Name, "Status": $Status}]')
+    }
+  done
+
+  if [[ "x${FORMAT}" == "xjson" ]]; then
+    output="{\"result\": 0, \"images\": ${images}}"
+    echo $output
+  else
+    seperator=---------------------------------------------------------------------------------------------------
+    rows="%-5s| %-50s| %s\n"
+    TableWidth=100
+    echo -e "\n\nVHI Images List"
+    printf "%.${TableWidth}s\n" "$seperator"
+    printf "%-5s| %-50s| %s\n" ID Name Status
+    printf "%.${TableWidth}s\n" "$seperator"
+
+    for row in $(echo "${subnets}" | jq -r '.[] | @base64'); do
+      _jq() {
+        echo "${row}" | base64 --decode | jq -r "${1}"
+      }
+      id=$(_jq '.id')
+      Name=$(_jq '.Name')
+      Status=$(_jq '.Status')
+      printf "$rows" "$id" "$Name" "$Status"
+    done
+  fi
+
 }
 
 getSubnets(){
@@ -150,7 +198,7 @@ getSubnets(){
     seperator=---------------------------------------------------------------------------------------------------
     rows="%-5s| %-20s| %-50s| %s\n"
     TableWidth=100
-    echo "VHI Cluster Subnets"
+    echo -e "\n\nVHI Cluster Subnets"
     printf "%.${TableWidth}s\n" "$seperator"
     printf "%-5s| %-20s| %-50s| %s\n" ID Name Network Subnet
     printf "%.${TableWidth}s\n" "$seperator"
@@ -181,23 +229,33 @@ responseValidate(){
 configure(){
   for i in "$@"; do
     case $i in
-      --vhi-proj-name=*)
-      VHI_PROJ_NAME=${i#*=}
+      --project-domain=*)
+      PROJECT_DOMAIN=${i#*=}
       shift
       shift
       ;;
-      --vhi-username=*)
-      VHI_USERNAME=${i#*=}
+      --user-domain=*)
+      USER_DOMAIN=${i#*=}
       shift
       shift
       ;;
-      --vhi-password=*)
-      VHI_PASSWORD=${i#*=}
+      --project=*)
+      PROJECT=${i#*=}
       shift
       shift
       ;;
-      --vhi-url=*)
-      VHI_URL=${i#*=}
+      --username=*)
+      USERNAME=${i#*=}
+      shift
+      shift
+      ;;
+      --password=*)
+      PASSWORD=${i#*=}
+      shift
+      shift
+      ;;
+      --url=*)
+      URL=${i#*=}
       shift
       shift
       ;;
@@ -211,12 +269,12 @@ configure(){
     esac
   done
 
-  echo "export OS_PROJECT_DOMAIN_NAME=${VHI_PROJ_NAME}" > ${VAP_ENVS};
-  echo "export OS_USER_DOMAIN_NAME=${VHI_PROJ_NAME}" >> ${VAP_ENVS};
-  echo "export OS_PROJECT_NAME=${VHI_PROJ_NAME}" >> ${VAP_ENVS};
-  echo "export OS_USERNAME=${VHI_USERNAME}" >> ${VAP_ENVS};
-  echo "export OS_PASSWORD=${VHI_PASSWORD}" >> ${VAP_ENVS};
-  echo "export OS_AUTH_URL=${VHI_URL}" >> ${VAP_ENVS};
+  echo "export OS_PROJECT_DOMAIN_NAME=${PROJECT_DOMAIN}" > ${VAP_ENVS};
+  echo "export OS_USER_DOMAIN_NAME=${USER_DOMAIN}" >> ${VAP_ENVS};
+  echo "export OS_PROJECT_NAME=${PROJECT}" >> ${VAP_ENVS};
+  echo "export OS_USERNAME=${USERNAME}" >> ${VAP_ENVS};
+  echo "export OS_PASSWORD=${PASSWORD}" >> ${VAP_ENVS};
+  echo "export OS_AUTH_URL=${URL}" >> ${VAP_ENVS};
   echo "export OS_IDENTITY_API_VERSION=3" >> ${VAP_ENVS};
   echo "export OS_AUTH_TYPE=password" >> ${VAP_ENVS};
   echo "export OS_INSECURE=true" >> ${VAP_ENVS};
@@ -226,12 +284,17 @@ configure(){
   echo "export OS_PLACEMENT_API_VERSION=1.22" >> ${VAP_ENVS};
   echo "export VAP_STACK_NAME=${VAP_STACK_NAME}" >> ${VAP_ENVS};
 
+  getInfraFlavors
+  getUserFlavors
+  getSubnets
+  getImages
+
 }
 
 case ${1} in
     configure)
-        configure "$@"
-        ;;
+      configure "$@"
+      ;;
 
     getSubnets)
       getSubnets
@@ -244,4 +307,9 @@ case ${1} in
     getUserFlavors)
       getUserFlavors
       ;;
+
+    getImages)
+      getImages
+      ;;
+
 esac
